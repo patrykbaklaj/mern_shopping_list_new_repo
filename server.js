70d22cdb90c6888744989fd3503d3c4aa90cbad1
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2019-06-17 14:57:19 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const items = require('./routes/api/items');
-
+const path = require("path");
+const items = require("./routes/api/items");
 
 const app = express();
 
 //body-parser midleware
 app.use(bodyParser.json());
 
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // connect to mongo
 mongoose
-.connect(db,  { useNewUrlParser: true })
-.then(() => console.log("MongoDB Connected....."))
-.catch(err => console.log(err));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected....."))
+    .catch(err => console.log(err));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // SET static folder
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 //use routes
-app.use('/api/items', items);
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
