cb02c316a82bb6eab87104e6ac024dd000ae5db4
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2019-06-18 22:55:11 +0100

    Modify server

--- server.js
+++ server.js
@@ -17,18 +17,18 @@ mongoose
     .then(() => console.log("MongoDB Connected....."))
     .catch(err => console.log(err));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-
 //use routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
